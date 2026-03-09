38b8d9d37be0e30cfc6606cff185d3529ff88d98
Adnane13-debug <adnaneazouigui@gmail.com>
2026-03-09 00:17:22 +0000

    feat: add authentification API ( register, login, getMe, logout) with JWT and bcrypt

---
+++ backend/middleware/auth.js
@@ -0,0 +1,30 @@
+// this file checks if the user is logged in
+// if token is valid → continue
+// if not → block the request
+
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const verifyToken = (req, res, next) => {
+
+  // get the token from the header
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  // if no token → block
+  if (!token) {
+    return res.status(401).json({ message: 'No token, access denied' })
+  }
+
+  // if token exists → verify it
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decoded // save user info for next steps
+    next() // continue to the route
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid token' })
+  }
+}
+
+export default verifyToken
---
+++ backend/middleware/roles.js
@@ -0,0 +1,17 @@
+// this file checks if the user has the right role
+// example: admin, membre
+
+const checkRole = (role) => {
+  return (req, res, next) => {
+
+    // if user role does not match → block
+    if (req.user.role !== role) {
+      return res.status(403).json({ message: `Access denied. ${role}s only.` })
+    }
+
+    // if role matches → continue
+    next()
+  }
+}
+
+export default checkRole
